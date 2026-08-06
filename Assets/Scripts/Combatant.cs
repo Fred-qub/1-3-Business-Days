5c135517d2755b36c63ff897f122c580df05f0ec
Fred-qub <fwilliamson01@qub.ac.uk>
2026-08-06 16:28:08 +0100

    added some placeholder sprites and combatant stats

---
+++ Assets/Scripts/Combatant.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class Combatant : MonoBehaviour
+{
+    public string combatantName;
+    
+    public int maxHP;
+    public int currentHP;
+    
+    public int initiative;
+    
+    
+}
